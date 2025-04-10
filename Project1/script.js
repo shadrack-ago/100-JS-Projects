5595c58f9cae24b5f1fb7ab4713d3db3aa3fee6a
Shadrack <shadrackmeshack7@gmail.com>
2025-04-10 22:58:19 +0300

    updates

--- Project1/script.js
+++ Project1/script.js
@@ -1,12 +1,12 @@
 const testimonials = [
     {
-      name: "Cherise G",
+      name: "Shady AGO",
       photoUrl:
-        "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+        "images/Shedy.jpg",
       text: "This is simply unbelievable! I would be lost without Apple. The very best. Not able to tell you how happy I am with Apple.",
     },
     {
-      name: "Rosetta Q",
+      name: "Jeff Bezos",
       photoUrl:
         "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
       text: "I would also like to say thank you to all your staff. Wow what great service, I love it! Apple impressed me on multiple levels.",
