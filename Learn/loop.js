bf52782bcb2e9a206995eb0c81b441d2f374c5b5
Shadrack <shadrackmeshack7@gmail.com>
2025-04-27 09:56:04 +0300

    Break statement

--- Learn/loop.js
+++ Learn/loop.js
@@ -17,3 +17,11 @@ const numbers = [1,2,3,4,5]
 numbers.forEach((number,index)=>{
     console.log(`index: ${index}, number: ${number}`)
 })
+
+// learning break and continue
+for(let i=1; i<=10; i++){
+    if(i===5){
+        break; // exit the loop when i is 5
+    }
+    console.log(`${i}`); // prints 1, 2, 3, 4
+}
