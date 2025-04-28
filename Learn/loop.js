82a6c8cac91dc5b0d281b382f2cf75b5fa5f7a54
Shadrack <shadrackmeshack7@gmail.com>
2025-04-28 17:39:03 +0300

    learnt adding elements

--- Learn/loop.js
+++ Learn/loop.js
@@ -34,3 +34,11 @@ for(let i=1; i<=10; i++){
     }
     console.log(i); // prints 1, 2, 3, 4
 }
+
+// nested for loop
+
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= 5; j++) {
+      console.log(`${i} x ${j} = ${i * j}`);
+    }
+  }
